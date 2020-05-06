2b2fa72a822c8c6eb842b2ed220eb93395559fa4
Ivan.Malinin <yjn.malinin@gmail.com>
2020-05-06 12:50:01 +0400

    Старт проекта

---
+++ ui5-web-components-application/src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "@ui5/webcomponents/dist/Button";
+import "@ui5/webcomponents/dist/Card";
+import "@ui5/webcomponents-fiori/dist/ShellBar";
+import "@ui5/webcomponents/dist/Avatar.js";
+import "@ui5/webcomponents/dist/ComboBox";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+      <ui5-shellbar
+primary-title="Орг.Структура"
+secondary-title="( Презентация )"
+logo="asset/img/sap.svg"
+show-co-pilot
+show-product-switch
+show-notifications
+notification-count="16"
+>
+
+
+
+	<ui5-avatar slot="profile" image="asset/img/iam.png"></ui5-avatar>
+
+</ui5-shellbar>
+
+      </header>
+      
+      {/* <ui5-button>Привет!!</ui5-button> */}
+    </div>
+  );
+}
+
+export default App;
